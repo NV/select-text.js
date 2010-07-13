f5670726c7649c6d93f131c216cb0ec61e80cb06
Nikita Vasilyev <me@elv1s.ru>
2010-07-13 17:48:36 +0400

    WebKit code style.

--- element_select.js
+++ element_select.js
@@ -2,67 +2,64 @@
  * @param {Number} [start=0] >= 0
  * @param {Number} [end=element.length] >= 0
  */
-Element.prototype.select = function select(start, end) {
-	start = start || 0;
-	if (arguments.length < 2) {
-		end = this.textContent.length;
-	} else {
-		end = Math.min(end, this.textContent.length);
-	}
+Element.prototype.select = function select(start, end)
+{
+    start = start || 0;
 
-	var range = document.createRange();
-	var node = this.firstChild;
+    if (arguments.length < 2)
+        end = this.textContent.length;
+    else
+        end = Math.min(end, this.textContent.length);
 
-	if (node) {
+    var range = document.createRange();
+    var node = this.firstChild;
 
-		var i = start;
-		var length;
+    if (node) {
 
-		while (i > (length = node.textContent.length)) {
-			i -= length;
-			node = node.nextSibling;
-		}
+        var i = start;
+        var length;
 
-		var startNode;
-		if (node.firstChild) {
-			startNode = node.firstChild;
-			while (startNode.firstChild) {
-				startNode = startNode.firstChild;
-			}
-		} else {
-			startNode = node;
-		}
+        while (i > (length = node.textContent.length)) {
+            i -= length;
+            node = node.nextSibling;
+        }
 
-		range.setStart(startNode, i);
+        var startNode;
+        if (node.firstChild) {
+            startNode = node.firstChild;
+            while (startNode.firstChild)
+                startNode = startNode.firstChild;
 
-		var j = end - (start - i);
-		while (j > (length = node.textContent.length)) {
-			j -= length;
-			node = node.nextSibling;
-		}
+        } else
+            startNode = node;
 
-		var endNode;
-		if (node.firstChild) {
-			endNode = node.firstChild;
-			while (endNode.firstChild) {
-				endNode = endNode.firstChild;
-			}
-		} else {
-			endNode = node;
-		}
+        range.setStart(startNode, i);
 
-		range.setEnd(endNode, j);
+        var j = end - (start - i);
+        while (j > (length = node.textContent.length)) {
+            j -= length;
+            node = node.nextSibling;
+        }
 
-	} else {
+        var endNode;
+        if (node.firstChild) {
+            endNode = node.firstChild;
+            while (endNode.firstChild)
+                endNode = endNode.firstChild;
+            
+        } else
+            endNode = node;
 
-		range.setStartBefore(this);
-		range.setEndBefore(this);
+        range.setEnd(endNode, j);
 
-	}
+    } else {
+        range.setStartBefore(this);
+        range.setEndBefore(this);
+    }
 
-	var selection = getSelection();
-	selection.removeAllRanges();
-	selection.addRange(range);
+    var selection = getSelection();
+    selection.removeAllRanges();
+    selection.addRange(range);
 
-	return this;
+    return this;
 };
